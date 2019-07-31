34f5d1181c7ba79fd2cef6e8375224d6924e4b91
Chris Hergert <chris3623@yahoo.com>
2019-07-31 16:59:06 -0500

    Add files via upload
    Add the query for analysis on only the top 20% of JCR Journals by holdings

---
+++ Analysis - Top 20% by Eigen.sql
@@ -0,0 +1,46 @@
+Select	mp.[JCR Category],
+		COUNT([ISSN Match]) as 'JCR Top Quintile Overlap Count in Holdings',
+		count([Journal Title]) as 'JCR Top Quintile Total Count',
+		CONVERT(VARCHAR(10),CAST(ROUND(COUNT([ISSN Match]) * 100.0 / count([Journal Title]), 2,1) AS decimal(5,2))) + '%' AS 'Top 20% Overlap Rate',
+		SUM([JCt]) as 'Total Cites',
+		ROUND(AVG([Normalized Eigenfactor]), 3) as 'Average Eigenfactor score'
+FROM
+(
+	SELECT	*,
+			row_number() over (partition by [JCR Category] order by [JCR Category],[Normalized Eigenfactor] DESC) as seqnum,
+			count([Journal Title]) over (partition by [JCR Category]) as cnt
+	From
+	(
+		SELECT	Categories.Category AS 'JCR Category',
+				Journals.[Journal Title],
+				m.ISSN as 'ISSN Match',
+				Journals.[Total Cites] as 'JCt',
+				Journals.[Article Influence Score],
+				Journals.[Journal Impact Factor],
+				Categories.[Aggregate Immediacy Index],
+				Categories.[Aggregate Impact Factor],
+				Categories.[# Journals] AS 'JCR Category Journal Count',
+				Categories.[Articles] AS 'JCR Category Article Count',
+				Categories.[Median Impact Factor],
+				Journals.[Normalized Eigenfactor]
+		FROM
+		(
+			Select DISTINCT Journals.[Journal Title], Journals.ISSN, Journals.JournID, Journals.[Article Influence Score] ,Journals.[Total Cites],Journals.[Journal Impact Factor], Journals.[Normalized Eigenfactor]
+			FROM JCR.dbo.Journals
+			INNER JOIN EJournals.dbo.EJournals
+				ON EJournals.dbo.EJournals.ISSN = Journals.ISSN
+			UNION
+			Select DISTINCT Journals.[Journal Title], Journals.ISSN, Journals.JournID, Journals.[Article Influence Score] ,Journals.[Total Cites],Journals.[Journal Impact Factor], Journals.[Normalized Eigenfactor]
+			FROM JCR.dbo.Journals
+			INNER JOIN EJournals.dbo.EJournals
+				ON EJournals.dbo.EJournals.eISSN = Journals.ISSN
+			WHERE EJournals.dbo.EJournals.eISSN <> EJournals.dbo.EJournals.ISSN
+		) m
+		RIGHT JOIN	Journals			ON Journals.ISSN = m.ISSN
+		LEFT JOIN	categories_journals ON Journals.JournID = categories_journals.Journ_ID
+		LEFT JOIN	Categories			ON categories_journals.Cat_ID = Categories.CatID
+	) mt
+) mp
+WHERE seqnum <= 0.2*cnt
+GROUP BY [JCR Category]
+ORDER BY [JCR Category]
